73d60e05bff10a4578f4d6de0ee8258053f8d6df
fsuser <fsuser@e2e-95-35.ssdcloudindia.net>
2023-07-03 23:19:23 +0530

    add pitch shift augmentation

--- audiomentations/augmentations/pitch_shift.py
+++ audiomentations/augmentations/pitch_shift.py
@@ -12,7 +12,14 @@ class PitchShift(BaseWaveformTransform):
 
     supports_multichannel = True
 
-    def __init__(self, min_semitones=-4, max_semitones=4, p=0.5):
+    def __init__(
+        self, min_semitones: float = 3.0, max_semitones: float = 4, p: float = 0.5
+    ):
+        """
+        :param min_semitones: Minimum semitones to shift. Negative number means shift down.
+        :param max_semitones: Maximum semitones to shift. Positive number means shift up.
+        :param p: The probability of applying this transform
+        """
         super().__init__(p)
         assert min_semitones >= -12
         assert max_semitones <= 12
@@ -20,33 +27,23 @@ class PitchShift(BaseWaveformTransform):
         self.min_semitones = min_semitones
         self.max_semitones = max_semitones
 
-    def randomize_parameters(self, samples, sample_rate):
+    def randomize_parameters(self, samples: np.ndarray, sample_rate: int):
         super().randomize_parameters(samples, sample_rate)
         if self.parameters["should_apply"]:
             self.parameters["num_semitones"] = random.uniform(
                 self.min_semitones, self.max_semitones
             )
 
+    
     def apply(self, samples, sample_rate):
-        try:
-            pitch_shifted_samples = librosa.effects.pitch_shift(
-                samples, sr=sample_rate, n_steps=self.parameters["num_semitones"]
-            )
-        except librosa.util.exceptions.ParameterError:
-            warnings.warn(
-                "Warning: You are probably using an old version of librosa. Upgrade"
-                " librosa to 0.9.0 or later for better performance when applying"
-                " PitchShift to stereo audio."
-            )
-            # In librosa<0.9.0 pitch_shift doesn't natively support multichannel audio.
-            # Here we use a workaround that simply loops over the channels instead.
-            # TODO: Remove this workaround when we remove support for librosa<0.9.0
-            pitch_shifted_samples = np.copy(samples)
-            for i in range(samples.shape[0]):
-                pitch_shifted_samples[i] = librosa.effects.pitch_shift(
-                    pitch_shifted_samples[i],
-                    sr=sample_rate,
-                    n_steps=self.parameters["num_semitones"],
-                )
 
-        return pitch_shifted_samples
+        random_num = random.randint(0, samples.shape[1] - sample_rate)
+        print(random_num)
+        samples_torch = np.copy(samples)
+        
+        samples_new = librosa.effects.pitch_shift(samples_torch[0, random_num: random_num + sample_rate],
+                                n_steps =self.parameters["num_semitones"],
+                                sr=sample_rate
+                                )
+        samples_torch[0, random_num: random_num + sample_rate] = samples_new
+        return samples_torch
